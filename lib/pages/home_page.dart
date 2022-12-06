64ecfc881d79e993308eafedbe21eea50aa7acbb
Dka-11 <adikaahmad05@gmail.com>
2022-12-06 21:47:09 +0700

    Add ListView widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nav & Rute - Adika - 2041720171"),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            // Masih tidak dapat di klik, for add action use InkWell Widget
+            // Widget GestureDoctor umum / bisa digunakan untuk selain gesture sentuhan.
+            return InkWell(
+              // InkWell memberikan efek ditekan.
+              onTap: () {
+                Navigator.pushNamed(context, '/item');
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(child: Text(item.name)),
+                      Expanded(
+                          child: Text(
+                        item.price.toString(),
+                        textAlign: TextAlign.end,
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
