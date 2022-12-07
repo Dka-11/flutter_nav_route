a3d882d2fc7fd434f41aec3d038b6c06d1ed7c5c
Dka-11 <adikaahmad05@gmail.com>
2022-12-07 07:54:04 +0700

    read then show data model

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
   const ItemPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Item Page"),
-      ),
-      body: const Center(
-        child: Text("This is new Page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Item Page"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(itemArgs.name),
+                const Text(" with "),
+                Text(itemArgs.price.toString())
+              ],
+            )
+          ],
+        ));
   }
 }
